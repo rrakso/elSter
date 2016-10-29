3c084533ce64f093c298b7c573a2b6c30b0c8466
rrakso <oskarrro90@gmail.com>
2016-10-29 23:29:06 +0200

    Popcorn
    tak

---
+++ elSter_mowca.ino
@@ -0,0 +1,340 @@
+/* _____ Sterowanie elWire modul glowny. _____ v2.1
+
+   Modul nasluchuje na porcie szeregowym komend
+   i nastepnie je wysyla, po otrzymaniu odpowiedzi/bledu zwraca ja.
+
+   Podlaczenie NRF24L01+
+     1 - GND
+     2 - VCC 3.3V !!! NOT 5V
+     3 - CE to Arduino pin 9
+     4 - CSN to Arduino pin 10
+     5 - SCK to Arduino pin 13
+     6 - MOSI to Arduino pin 11
+     7 - MISO to Arduino pin 12
+     8 - UNUSED
+
+    Nazwa modulu: 1
+    Adres modulu: 0xE8E8F0F0E1
+
+   komendy:
+   ---------
+   99 - ustawienia modulu
+   -- 1 zmiana adresu lokalnego 99,1,<ID>
+   -- 2 restart do ustawień fabrycznych 99,2
+   -- 3 restart 99,3
+   ---------
+   wyslijKomende(A, B, C, D);
+   A - do kogo [0-4]
+   B - komenda:
+   - 1 - wlacz el wire numer C (domyslnie C = 1)
+   - 0 - wylacz elwire numer C (domyslnie C = 1)
+   - 2 - miganie, wymaga parametrow: C (czas swiecenia w ms lub wlacz/wylacz miganie(T/N) i D (czas przerwy w ms) (jeżeli C=T, wlacza zapisane* miganie; C=N wyłącza zapisane* miganie)
+   - 3 - sprawdz poziom naladowania baterii, wymaga parametrow C i D lub C (C=X: BAT_X; [zwr. poziom nal. bat. X],   C=X i D=Y: BAT_X BAT_Y; [zwr. poziomy nal. bat. X i Y])
+   - 4 - jasnosc PWM, C - numer elwie D - jasnosc (dokonczyc)
+
+   miganie* - domyslne czasy to 100ms wlaczone 500ms wylaczone
+
+   By: Oskar & Patryk 2016
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <printf.h> //printf_begin();
+#include <RF24.h>
+#include <RF24_config.h>
+#include <EEPROM.h>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <ctype.h>
+
+uint64_t pipe;
+const int CE_PIN = 9;
+const int CSN_PIN = 10;
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+int ID;
+const int adresID = 1 + sizeof(pipe);
+
+
+//#define numUsers (sizeof(users)/sizeof(char *)) //array size
+#define MAX_ID_ONLINE 40
+unsigned int onLineID[MAX_ID_ONLINE];
+
+void setup() {
+  //attachInterrupt(digitalPinToInterrupt(2), sprawdzAlarm, FALLING); //przerywanie na pinie 2
+  Serial.setTimeout(10);
+  Serial.begin(9600);
+
+  for (unsigned int ok = 0; ok < MAX_ID_ONLINE; ok++) {
+    onLineID[ok] = 0;
+  }
+
+  EEPROM.get(0, pipe);
+  EEPROM.get(adresID, ID);
+  if (pipe == 0 || pipe == -1) {
+    uint64_t adresPoczatkowyNRF = 0xE8E8F0F0E1LL;
+    ID = 1;
+    EEPROM.put(0, adresPoczatkowyNRF);  // zapisz fabryczny adres
+    EEPROM.put(adresID, ID);            // zapisz fabryczne ID
+    EEPROM.get(0, pipe);
+  }
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setRetries(0, 0);
+  radio.disableCRC();
+  radio.setAutoAck(false);
+
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  printf_begin();
+  radio.printDetails();
+  //radio.get_status(); //nie wiem jak to obsluzyc
+  //Serial.println("Kto zyje?");
+  //wyslijPING(); //PING
+}
+
+void loop() {
+  if (radio.available()) {
+    Serial.println("OKOKOK!");
+    int daneOdebrane[4];
+    radio.read(daneOdebrane, sizeof(daneOdebrane));
+
+    if (daneOdebrane[0] == 1) { //Informacja (0-1024) o poziomie baterii w 4 (BAT_4_232)
+      Serial.print("BAT_"); Serial.print(daneOdebrane[1]); Serial.print("_"); Serial.print(daneOdebrane[2]); Serial.print("_"); Serial.print(daneOdebrane[3]); Serial.println(";");
+    } else if (daneOdebrane[0] == 2) {
+      Serial.print("ER_");
+      switch (daneOdebrane[2]) {
+        case 1: //Alarm niski poziom baterii w 2 (ER_BAT_NP_2)
+          Serial.print("BAT_NP_"); Serial.print(daneOdebrane[1]); Serial.println(";");
+          break;
+        case 2: //Alarm blad zapisu do eepromu ADRESU
+          Serial.print(daneOdebrane[1]); Serial.print("_EEPROM_ADDRESS_"); Serial.print(daneOdebrane[3]); Serial.println(";");
+        //Miejsce na kolejne bledy
+        case 3: //Alarm blad zapisu do eepromu PCF
+          Serial.print(daneOdebrane[1]); Serial.print("_EEPROM_PCF_"); Serial.print(daneOdebrane[3]); Serial.println(";");
+          //Miejsce na kolejne bledy
+      }
+    } else if (daneOdebrane[0] == 3) { //Wiadomosc od modulu
+      if (daneOdebrane[2] == 98) { //meldunek slave o zakonczeniu inicjalizacji
+        manageActiveID(true, daneOdebrane[1]); //zapisz ID
+      } else if (daneOdebrane[2] == 99) {
+        manageActiveID(false, daneOdebrane[1]); //usun ID
+      }
+      Serial.print("MSG_"); Serial.print(daneOdebrane[1]); Serial.print("_"); Serial.print(daneOdebrane[2]); Serial.print("_"); Serial.print(daneOdebrane[3]); Serial.println(";"); //Wiadomosc od 3 o tresci wiadomosc (MSG_3_WIADOMOSC) jezeli wiadomosc = 1: OK
+    }
+  }
+
+  if (Serial.available()) {
+    int rem, t0, t1, t2;
+    rem = Serial.parseInt();
+    if (rem == 99) {
+      ustawienia();
+    } else {
+      t0 = Serial.parseInt();
+      t1 = Serial.parseInt();
+      t2 = Serial.parseInt();
+      bool wyslano = wyslijKomende(rem, t0, t1, t2);
+      //Serial.print("SEND_"); Serial.println(wyslano); //SEND_TRUE / SEND_FALSE
+    }
+  }
+}
+
+
+void wyslijPING() {
+  radio.stopListening();
+  radio.openWritingPipe(0xE8E8F0F000);
+  int komenda[3] = {7, 0, 0};
+  radio.write(komenda, sizeof(komenda));
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  int timeout = 0;
+  while (!radio.available()) {
+    delay(500);
+    timeout += 500;
+    //Serial.print("WAIT: "); Serial.print(timeout); Serial.println("ms.");
+    if (timeout == 2000) {
+      Serial.println("TIMEOUT!!!");
+      break;
+    }
+  }
+  //  while (radio.available()) {
+  //    int daneOdebrane[4];
+  //    radio.read(daneOdebrane, sizeof(daneOdebrane));
+  //    Serial.println("Zapis do bufora");
+  //    zameldowane += String(daneOdebrane[1], DEC) + ";";
+  //    Serial.println(zameldowane);
+  //    Serial.print("PONG: "); Serial.println(daneOdebrane[1]);
+  //  }
+}
+
+bool wyslijKomende(int remote, int k1, int k2, int k3) {
+  bool stan = true;
+  int dane[3];
+
+  radio.stopListening();
+  radio.openWritingPipe(zrobAdres(remote + 224));
+  dane[0] = k1;
+  dane[1] = k2;
+  dane[2] = k3;
+  //Serial.println(remote - 1);
+  if (!radio.write(dane, sizeof(dane))) {
+    stan = false;
+  }
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  return stan;
+}
+
+uint64_t zrobAdres(long int koncowka) {
+  uint64_t accumulator = 0;
+
+  String koncowkaObliczona = String(koncowka, HEX);
+  String adres = "e8e8f0f0" + koncowkaObliczona;
+  // Length (with one extra character for the null terminator)
+  int str_len = adres.length() + 1;
+  // Prepare the character array (the buffer)
+  //char adresSpoko[30]; //nie wiem jak sprawdzic rozmiar maksymalnego adresu, a 30 dziala xD
+  char adresSpoko[str_len];
+  adres.toCharArray(adresSpoko, str_len);
+
+  for (size_t i = 0 ; isxdigit((unsigned char)adresSpoko[i]) ; ++i)
+  {
+    char c = adresSpoko[i];
+    accumulator *= 16;
+    if (isdigit(c)) /* '0' .. '9'*/
+      accumulator += c - '0';
+    else if (isupper(c)) /* 'A' .. 'F'*/
+      accumulator += c - 'A' + 10;
+    else /* 'a' .. 'f'*/
+      accumulator += c - 'a' + 10;
+
+  }
+  return accumulator;
+}
+
+void ustawienia() {
+  //Serial.println("USTAWIENIA:");
+  //Serial.println("1. zmiana adresu (np. 1,<adres> 1,4)");
+  //Serial.println("2. Ustawienia fabryczne;)");
+  int opcja = Serial.parseInt();
+  long int argument;
+  switch (opcja) {
+    case 1:
+      //Serial.println();
+      argument = Serial.parseInt();
+      //Serial.print("Zmiana adresu z "); Serial.print(ID); Serial.print(" na "); Serial.print(argument);
+      EEPROM.put(0, zrobAdres(argument + 224));
+      EEPROM.get(0, pipe);
+      EEPROM.put(adresID, argument);
+      EEPROM.get(adresID, ID);
+      if ((pipe == zrobAdres(argument + 224)) && (ID == argument)) {
+        //pipe = zrobAdres(argument + 224); // to nie dzialalo
+        Serial.println(": OK!");
+        radio.stopListening();
+        radio.openReadingPipe(1, pipe);
+        radio.startListening();
+      } else {
+        Serial.println(": NIEPOWODZENIE!");
+        //Serial.println("Blad zapisu do EEPROMU - ER01E");
+      }
+      break;
+    case 2:
+      //Serial.print("Przywracanie ustawien fabrycznych, czekaj...");
+      for ( int i = 0 ; i < EEPROM.length() ; i++ )
+        EEPROM.write(i, 0);
+      Serial.println("... OK!");
+      //Serial.println("RESET ZA 2s!");
+      //Serial.println("");
+      //delay(2000);
+      //Serial.println("RESET SZMATY!");
+      arduinoReset();
+      //Automatyczne restart potrzebny, wykonać pogramowo (sprzętowo)
+      break;
+    case 3:
+      Serial.println();
+      Serial.println("RESET!!!");
+      Serial.println();
+      arduinoReset();
+      break;
+    case 4:
+      wyslijPING();
+      //Serial.println("PING WYSLANY!");
+      break;
+    case 5:
+      Serial.println(zwrocAktywneID());
+      break;
+    case 6:
+      argument = Serial.parseInt();
+      manageActiveID(true, argument);
+      break;
+    case 7:
+      argument = Serial.parseInt();
+      manageActiveID(false, argument);
+      break;
+  }
+}
+
+void arduinoReset() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW); //Arduino umiera :c
+}
+
+String zwrocAktywneID() {
+  bool zameldowaneJuz = false;
+  String zameldowane = "ACT:";
+  for (unsigned int husky = 0; husky < MAX_ID_ONLINE; husky++) {
+    if (onLineID[husky] != 0) {
+      if (zameldowaneJuz) {
+        zameldowane += ",";
+      }
+      zameldowane += String(onLineID[husky], DEC);
+      zameldowaneJuz = true;
+    }
+  }
+  zameldowane += ";";
+  return zameldowane;
+}
+
+void manageActiveID(bool funkcja, int ID) {
+  if (!czyIstnieje(ID) && funkcja) {
+    unsigned int k = 0;
+    while (1) {
+      if (k == MAX_ID_ONLINE) {
+        Serial.print("ER_ZAP_ID_"); Serial.println(ID);
+        return;
+      } else if (onLineID[k] == 0) {
+        onLineID[k] = ID;
+        return;
+      } else {
+        k++;
+      }
+    }
+  } else if (czyIstnieje(ID) && !funkcja) {
+    unsigned int k = 0;
+    while (1) {
+      if (k == MAX_ID_ONLINE) {
+        Serial.print("ER_USN_ID_"); Serial.println(ID);
+        return;
+      } else if (onLineID[k] == ID) {
+        onLineID[k] = 0;
+        return;
+      } else {
+        k++;
+      }
+    }
+  }
+}
+
+bool czyIstnieje(unsigned int ID) {
+  unsigned int k = 0;
+  while (1) {
+    if (k == MAX_ID_ONLINE) {
+      return false;
+    } else if (onLineID[k] == ID) {
+      return true;
+    } else {
+      k++;
+    }
+  }
+}
---
+++ elSter_slave.ino
@@ -0,0 +1,433 @@
+/* _____ Sterowanie elWire modul niewolnika. _____ v2.0
+ *
+ * Modul nasluchuje na transmiterze komend
+ * i nastepnie je odbiera, i interpretuje.
+ *
+ * Podlaczenie NRF24L01+
+ *   1 - GND
+ *   2 - VCC 3.3V !!! NOT 5V
+ *   3 - CE to Arduino pin 8
+ *   4 - CSN to Arduino pin 7
+ *   5 - SCK to Arduino pin 13
+ *   6 - MOSI to Arduino pin 11
+ *   7 - MISO to Arduino pin 12
+ *   8 - UNUSED
+ *
+ *  Nazwa m modulu: 0xE8E8F0F0E2
+ *odulu: 2
+ *  Adres
+ * By: Oskar & Patryk 2016
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <printf.h> //printf_begin();
+#include <RF24.h>
+#include <RF24_config.h>
+#include <EEPROM.h>
+#include <Wire.h>
+
+
+#include <stdio.h>  //do generowania adresow HEX
+#include <stdint.h> //do generowania adresow HEX
+#include <ctype.h>  //do generowania adresow HEX
+
+/////////////USTAWIENIA///////////////
+const int CE_PIN = 6;
+const int CSN_PIN = 5;
+//const int CE_PIN = 9;
+//const int CSN_PIN = 8;
+
+const int ledInfo = 4;
+
+int elWire[2] = {A4, A5};
+int bateria[2] = {A2, A1};
+
+int miganieS = 500; //domyslnie przerwa w swieceniu w ms
+int miganieN = 100; //domyslnie czas swiecenia w ms
+//////////////////////////////////////
+
+int master = 1;
+uint64_t pipe;
+bool PCF;
+int ID;
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+
+bool usrBttnZ = false;
+
+const int adresID = 1 + sizeof(pipe);
+const int adresPCF = 1 + sizeof(pipe) + sizeof(adresID);
+
+bool PCFaktywny[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+const int PCFadres[8] = {0x38, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27};
+byte PCF_DDR[8];
+byte PCF_PORT[8];
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), usrBttnZmienna, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), ostatnieSlowo, FALLING);
+  pinMode(ledInfo, OUTPUT);
+  for (int i = 0; i < 2; i++) {
+    pinMode(elWire[i], OUTPUT);
+  }
+  for (int i = 0; i < 2; i++) {
+    pinMode(bateria[i], INPUT);
+  }
+
+  EEPROM.get(0, pipe);
+  if (pipe == 0 || pipe == -1) {
+    master = 1;
+    uint64_t adresPoczatkowyNRF = 0xE8E8F0F0E2LL; //Adres 0xE8E8F0F0E2LL czyli 2
+    ID = 2;
+    PCF = false;
+    EEPROM.put(0, adresPoczatkowyNRF);  // zapisz fabryczny adres
+    EEPROM.put(adresID, ID);            // zapisz fabryczne ID
+    EEPROM.put(adresPCF, PCF);          // zapisz fabryczny stan funkcji PCF
+  }
+  EEPROM.get(0, pipe);
+  EEPROM.get(adresID, ID);
+  EEPROM.get(adresPCF, PCF);
+
+  digitalWrite(ledInfo, HIGH);
+  delay(200);
+  digitalWrite(ledInfo, LOW);
+  delay(100);
+
+  //Wire.begin();
+  radio.begin();
+
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setRetries(0, 0);
+  radio.disableCRC();
+  radio.setAutoAck(false);
+
+  radio.openReadingPipe(1, pipe);
+  radio.openReadingPipe(2, 0xE8E8F0F000); //broadcast
+  radio.startListening();
+  //printf_begin();
+  //radio.printDetails();
+
+  wyslijKomende(1, 3, ID, 97, PCF);
+  wyslijKomende(1, 3, ID, 98, pipe);
+}
+
+void loop() {
+  if (usrBttnZ) {
+    detachInterrupt(digitalPinToInterrupt(2));
+    usrBttn();
+    attachInterrupt(digitalPinToInterrupt(2), usrBttnZmienna, RISING);
+  }
+  if (radio.available()) {
+    int daneOdebrane[3];
+    radio.read(daneOdebrane, sizeof(daneOdebrane));
+
+    if ((daneOdebrane[0] == 1 && !PCF) || (daneOdebrane[0] == 0 && !PCF)) {
+      digitalWrite(elWire[daneOdebrane[1] - 1], daneOdebrane[0]); //Ustaw stan HIGH/LOW na pinie elWire[x] np. 1,3 / 0,3; 1,x / 0,x
+
+    } else if ((daneOdebrane[0] == 1 && PCF) || (daneOdebrane[0] == 0 && PCF)) {
+      if (PCFaktywny[daneOdebrane[1] - 1]) {
+        jebanyPCF(daneOdebrane[1], daneOdebrane[2], daneOdebrane[0]);
+      } else {
+        for (int o = 0; o < 8; o++)
+          jebanyPCFjako(daneOdebrane[1], o, OUTPUT);
+        PCFaktywny[daneOdebrane[1] - 1] = true;
+        jebanyPCF(daneOdebrane[1], daneOdebrane[2], daneOdebrane[0]);
+      }
+
+    } else if (daneOdebrane[0] == 2) {
+      if (daneOdebrane[1] == 5 && daneOdebrane[1] != 4) {
+        miganie(daneOdebrane[2]); //miganie, moze migac kilka paskow
+      } else {
+        miganieS = daneOdebrane[1]; //ustaw czas przerwy w swieceniu
+        miganieN = daneOdebrane[2]; //ustaw czas swiecenia
+        //wyslijKomende(0, 3, slaveNAME, 1); //OK
+      }
+
+    } else if (daneOdebrane[0] == 3) {
+      int pozNalBat1, pozNalBat2;
+      if (daneOdebrane[2] == 0) {
+        pozNalBat1 = analogRead(bateria[daneOdebrane[1] - 1]); //odczytaj poziom naladowania baterii bateria[x]
+        wyslijKomende(1, 1, ID, 1, pozNalBat1);
+      } else {
+        pozNalBat1 = analogRead(bateria[daneOdebrane[1] - 1]); //odcztaj poziomy naladowania dwoch baterii bateria[x] bateria[y]
+        pozNalBat2 = analogRead(bateria[daneOdebrane[2] - 1]);
+        wyslijKomende(1, 1, ID, 1, pozNalBat1);
+        wyslijKomende(1, 1, ID, 2, pozNalBat2);
+      }
+
+    } else if (daneOdebrane[0] == 4) { // zmiana adresu KM: 4,<nowy adres>[1-6]
+      EEPROM.put(0, zrobAdres(daneOdebrane[1] + 224));
+      EEPROM.get(0, pipe);
+      EEPROM.put(adresID, daneOdebrane[1]);
+      EEPROM.get(adresID, ID);
+      if ((pipe == zrobAdres(daneOdebrane[1] + 224)) && (ID == daneOdebrane[1])) {
+        wyslijKomende(master, 3, ID, 4, 1); //powiadom o prawidlowym wykoananiu komendy przesylajac swoje ID
+      } else {
+        wyslijKomende(master, 2, ID, 2, -1); // -1 powiadom o bledzie
+      }
+
+    } else if (daneOdebrane[0] == 5) { //wlaczenie/wylaczenie obslugi PCF8574 KM: 5,1 / 5,0
+      bool pcfTMP;
+      if (daneOdebrane[1] > 0) {//ogranicza wyobraznie Patyka
+        PCF = true;
+      } else {
+        PCF = false;
+      }
+      EEPROM.put(adresPCF, PCF);
+      EEPROM.get(adresPCF, pcfTMP);
+      if (pcfTMP == PCF) {
+        wyslijKomende(master, 3, ID, 5, PCF); //powiadom o prawidlowym wykoananiu komendy przesylajac stan funkcji PCF
+        wyslijKomende(master, 3, ID, 8, 1); //powiadom o resecie
+        powerReset();
+      } else {
+        wyslijKomende(master, 2, ID, 3, -1); //-1 powiadom o bledzie
+      }
+
+    } else if (daneOdebrane[0] == 6) { //wymazywanie danych z EEPROMu / ustawienia fabryczne KM: 6
+      przywrocUstawieniaFabryczne();
+
+    } else if (daneOdebrane[0] == 7) { //ping
+      wyslijKomende(master, 3, ID, 98, pipe);
+      digitalWrite(ledInfo, HIGH);
+      delay(500);
+      digitalWrite(ledInfo, LOW);
+
+    } else if (daneOdebrane[0] == 8) { //PowerReset
+      wyslijKomende(master, 3, ID, 8, 1); //powiadom o resecie
+      powerReset();
+
+    } else if (daneOdebrane[0] == 9) {
+      wyslijKomende(master, 3, ID, 9, 1);
+      digitalWrite(ledInfo, HIGH);
+      delay(2000);
+      digitalWrite(ledInfo, LOW);
+      delay(2000);
+      digitalWrite(ledInfo, HIGH);
+      delay(2000);
+      digitalWrite(ledInfo, LOW);
+    }
+  }
+}
+
+bool wyslijKomende(int remote, int k1, int k2, int k3, int k4) {
+  bool stan = true;
+  int dane[4];
+  radio.stopListening();
+  radio.openWritingPipe(zrobAdres(remote + 224));
+  dane[0] = k1;
+  dane[1] = k2;
+  dane[2] = k3;
+  dane[3] = k4;
+  if (!radio.write(dane, sizeof(dane))) {
+    stan = false;
+  }
+  radio.openReadingPipe(1, pipe);
+  radio.openReadingPipe(2, 0xE8E8F0F000); //broadcast
+  radio.startListening();
+  return stan;
+}
+
+uint64_t zrobAdres(long int koncowka) {
+  uint64_t accumulator = 0;
+  String koncowkaObliczona = String(koncowka, HEX);
+  String adres = "e8e8f0f0" + koncowkaObliczona;
+  //Dlugosc (+ jeden char dla null)
+  int str_len = adres.length() + 1;
+  //przygotuj char array (bufor)
+  //char adresSpoko[30]; //nie wiem jak sprawdzic rozmiar maksymalnego adresu, a 30 dziala xD
+  char adresSpoko[str_len];
+  adres.toCharArray(adresSpoko, str_len);
+
+  for (size_t i = 0 ; isxdigit((unsigned char)adresSpoko[i]) ; ++i) {
+    char c = adresSpoko[i];
+    accumulator *= 16;
+    if (isdigit(c)) /* '0' .. '9'*/
+      accumulator += c - '0';
+    else if (isupper(c)) /* 'A' .. 'F'*/
+      accumulator += c - 'A' + 10;
+    else /* 'a' .. 'f'*/
+      accumulator += c - 'a' + 10;
+
+  }
+  return accumulator;
+}
+
+void powerReset() {
+  pinMode(A3, OUTPUT);
+  digitalWrite(A3, HIGH);
+}
+
+void miganie(int ile) {
+  if (radio.available()) {
+    int daneOdebrane[3];
+    radio.read(daneOdebrane, sizeof(daneOdebrane));
+    if (daneOdebrane[0] == 2) {
+      if (daneOdebrane[1] != 5 && daneOdebrane[1] == 4) {
+        return; //przerwi miganie
+      } else {
+        miganieS = daneOdebrane[1]; //ustaw czas swiecenia
+        miganieN = daneOdebrane[2]; //ustaw czas przerwy w swieceniu
+      }
+    }
+  }
+  for (int i = 0; i < ile - 1; i++) {
+    digitalWrite(elWire[i], HIGH);
+  }
+  delay(miganieS);
+  for (int i = 0; i < ile - 1; i++) {
+    digitalWrite(elWire[i], LOW);
+  }
+  delay(miganieN);
+}
+
+void jebanyPCF(uint8_t ID, uint8_t pin, uint8_t stan) {
+  byte _PIN;
+  --ID;
+  uint8_t adres = PCFadres[ID];
+
+  /* Set PORT bit value */
+  if (stan)
+    PCF_PORT[ID] |= (1 << pin);
+  else
+    PCF_PORT[ID] &= ~(1 << pin);
+
+  /* Start request, wait for data and receive GPIO values as byte */
+  Wire.requestFrom(adres, (uint8_t) 0x01);
+  while (Wire.available() < 1)
+    ;
+  _PIN = Wire.read();
+
+  /* Update GPIO values */
+  /* Compute new GPIO states */
+  //uint8_t value = ((_PIN & ~PCF_DDR[addres]) & ~(~PCF_DDR[addres] & PCF_PORT[addres])) | PCF_PORT[addres]; // Experimental
+  uint8_t value = (_PIN & ~PCF_DDR[ID]) | PCF_PORT[ID];
+  /*Serial.println("jebany");
+  Serial.println(value, BIN);
+  Serial.println("pin");
+  Serial.println(_PIN, BIN);*/
+
+  /* Start communication and send GPIO values as byte */
+  Wire.beginTransmission(adres);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+void jebanyPCFjako(uint8_t ID, uint8_t pin, uint8_t mode) {
+  byte _PIN;
+  --ID;
+  uint8_t adres = PCFadres[ID];
+
+  /* Switch according mode */
+  switch (mode) {
+    case INPUT:
+      PCF_DDR[ID] &= ~(1 << pin);
+      PCF_PORT[ID] &= ~(1 << pin);
+      break;
+
+    case INPUT_PULLUP:
+      PCF_DDR[ID] &= ~(1 << pin);
+      PCF_PORT[ID] |= (1 << pin);
+      break;
+
+    case OUTPUT:
+      PCF_DDR[ID] |= (1 << pin);
+      PCF_PORT[ID] &= ~(1 << pin);
+      break;
+
+    default:
+      break;
+  }
+
+  /* Start request, wait for data and receive GPIO values as byte */
+  Wire.requestFrom(adres, (uint8_t) 0x01);
+  while (Wire.available() < 1)
+    ;
+  _PIN = Wire.read();
+
+  /* Update GPIO values */
+  /* Compute new GPIO states */
+  //uint8_t value = ((_PIN & ~PCF_DDR[addres]) & ~(~PCF_DDR[addres] & PCF_PORT[addres])) | PCF_PORT[addres]; // Experimental
+  uint8_t value = (_PIN & ~PCF_DDR[ID]) | PCF_PORT[ID];
+  /*Serial.println("JAKO");
+  Serial.println(value, BIN);
+  Serial.println("pin");
+  Serial.println(_PIN, BIN);*/
+
+  /* Start communication and send GPIO values as byte */
+  Wire.beginTransmission(adres);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+void ostatnieSlowo() {
+  wyslijKomende(master, 3, ID, 99, 1);
+}
+
+void usrBttnZmienna() {
+  usrBttnZ = true;
+}
+
+void usrBttn() {
+  int blink = 0;
+  //delay(200);
+  while (digitalRead(2)) { //migaj, zlicz ilosc migniec
+    digitalWrite(ledInfo, HIGH);
+    delay(250);
+    digitalWrite(ledInfo, LOW);
+    delay(250);
+    blink++;
+  }
+  
+  switch (blink) {
+    case 2:
+      wyslijKomende(master, 3, ID, 98, pipe);
+      digitalWrite(ledInfo, HIGH);
+      delay(500);
+      digitalWrite(ledInfo, LOW);
+      break;
+    case 5:
+      delay(250);
+      while (!digitalRead(2)) { //migaj szybko aby potwierdzic wymazanie danych
+        digitalWrite(ledInfo, HIGH);
+        delay(250);
+        digitalWrite(ledInfo, LOW);
+        delay(150);
+      }
+      float t0, czas;
+      t0 = millis();
+      while (digitalRead(2)) { //przycisniecie przycisku potwierdzi wymazanie, przytrzymanie przycisku powyzej 3s anuluje wymazanie
+        digitalWrite(ledInfo, HIGH);
+        czas = millis() - t0;
+        if (czas > 3000) {
+          digitalWrite(ledInfo, LOW);
+          delay(850);
+          exit;
+        }
+      }
+      if (czas < 3000) {
+        digitalWrite(ledInfo, LOW);
+        delay(600);
+        digitalWrite(ledInfo, HIGH);
+        delay(100);
+        digitalWrite(ledInfo, LOW);
+        delay(600);
+        digitalWrite(ledInfo, HIGH);
+        delay(100);
+        digitalWrite(ledInfo, LOW);
+        przywrocUstawieniaFabryczne();
+      }
+      break;
+  }
+  usrBttnZ = false;
+}
+
+void przywrocUstawieniaFabryczne() {
+  wyslijKomende(master, 3, ID, 6, 0); //powiadom o rozpoczęciu kasowania pamięci
+  for ( int i = 0 ; i < EEPROM.length() ; i++ )
+    EEPROM.write(i, 0);
+  wyslijKomende(master, 3, ID, 6, 1); //powiadom o zakonczeniu kasowania pamięci
+  powerReset();
+}
+
